3c6705136e15d7197cd96820d826fc98428b5a15
Kanchukommala vijay kumar <vijaykumar0911195@gmail.com>
2023-09-23 14:46:53 +0530

    update

--- basic/a.m
+++ basic/a.m
@@ -1,1 +1,10 @@
 t=0:1/1e3:1
+Fs = 1000;                      % samples per second
+t = 0:1/Fs:2-1/Fs;
+N = length(t);
+
+%%Frequency specifications:
+f = (-N/2:N/2-1)*Fs/N;           % hertz
+
+%%Sine wave:
+Fc = 50;                       % hertz
